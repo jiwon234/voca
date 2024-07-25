a009481a8219fe1c4acca6ddcd30373de7a9fc3c
jiwon234 <jiwon2716@gmail.com>
2024-07-25 15:50:27 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from "./component/Header";
+import DayList from "./component/DayList";
+import Day from "./component/Day";
+import EmptyPage from "./component/EmptyPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+ 
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes> {/* 이부분만 url에 따라 바뀝니다. */}
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
