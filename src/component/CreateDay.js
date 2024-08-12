2010e7c6f452de93e1e9606a5c360e6740c41d02
jiwon234 <145983750+jiwon234@users.noreply.github.com>
2024-08-13 00:55:12 +0900

    Enhance CreateDay with data refresh and conditional navigation
    메인화면 이동을 선택하지 않았을 때
    - Day 추가 후 현재일수의 리렌더링 필요함
    - Day 추가에서 오류 발생

--- src/component/CreateDay.js
+++ src/component/CreateDay.js
@@ -1,11 +1,13 @@
 import useFetch from "../hooks/useFetch"
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function CreateDay() {
     
     const days = useFetch("http://localhost:3001/days");
     const navigate = useNavigate();
+    const [goIndex, setGoIndex] = useState(false);
 
     function addDay() {
         fetch(`http://localhost:3001/days/`, {
@@ -20,14 +22,25 @@ export default function CreateDay() {
         }).then(res => {
             if(res.ok) {
                 alert("생성이 완료 되었습니다.");
-                navigate(`/`);
+                if (goIndex) {
+                    navigate(`/`);
+                }
             }
         });
     }
+
+    const handleCheckboxChange = (e) => {
+        setGoIndex(e.target.checked);
+        console.log('goIndex 상태가 변경되었습니다:', e.target.checked);
+    };
+
     return (
         <div className="m-3">
             <h3 className="text-lg mb-3 font-semibold">현재 일수: {days.length}일</h3>
-            <button onClick={addDay} className="btn">Day 추가</button>
+            <div className="flex flex-col">
+                <button onClick={addDay} className="btn w-28 mb-2">Day 추가</button>
+                <div className="flex flex-row"><input type="checkbox" checked={goIndex} onChange={handleCheckboxChange}/><div className="text-sm ms-1"> 추가 후 메인화면으로 이동</div></div>
+            </div>
         </div>
     )
 };
