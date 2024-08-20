e5f464b8b997a56397c61bfe5ba7d1e65af65616
jiwon234 <145983750+jiwon234@users.noreply.github.com>
2024-08-20 14:06:28 +0900

    (깃 취소) 오른쪽 하단 토글메뉴 간단 구현

--- src/component/Dictionary/DayList.js
+++ src/component/Dictionary/DayList.js
@@ -2,34 +2,12 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import QuizList from "../Quiz/QuizList";
-import { useParams } from "react-router-dom";
 
 export default function DayList() {
     const days = useFetch("http://localhost:3001/days");
 
-    const {day} = useParams();
-    const words = useFetch(`http://localhost:3001/words?day=${day}`);
-
     if (days.length === 0) {
         return <span>Loading...</span>;
-    }
-
-    function calculateProgress(temp) {
-        const dayItems = words.filter(item => item.day == temp) //item들 중에서 현재 day랑 같은 것만 뽑아냄
-        console.log(`dayItems: ${dayItems}`);
-        const doneItemsCount = dayItems.filter(item => item.isDone).length; //item.isDone이 true인 것만 length 구함 
-        console.log(`doneItemsCount: ${doneItemsCount}`);
-        
-        const totalItemsCount = dayItems.length; // day가 같은 것들의 length를 구합니다. 
-        console.log(`totalItemsCount: ${totalItemsCount}`);
-        const donePercentage = totalItemsCount > 0 ? (doneItemsCount / totalItemsCount) * 100: 0;
-        console.log(`Day에서 완료된 항목의 비율: ${donePercentage}%`);
-
-        return donePercentage;
-    }
-
-    function test1 (day) {
-        console.log(day);
     }
 
     return (
@@ -39,10 +17,7 @@ export default function DayList() {
                 {days.map(day => ( //days의 각 개체를 day라고 지칭 
                     <li key={day.id} className="h-24 rounded-lg shadow-lg hover:bg-gray-200 duration-100"> {/*key는 있는게 좋다네*/}
                         <Link to={`/day/${day.day}`}
-                        className="text-lg flex flex-col items-center justify-center h-full w-full progress">Day {day.day}
-                        <div className="mt-3 w-24 bg-gray-200 rounded-full h-1.5 dark:bg-gray-300">
-                            <div className="bg-blue-600 h-1.5 rounded-full" style={{width: `${calculateProgress(day.day)}%`}}></div>
-                        </div></Link>
+                        className="text-lg flex items-center justify-center h-full w-full">Day {day.day}</Link>
                     </li>
                 ))}
             </ul>
--- src/component/FloatingNav.js
+++ src/component/FloatingNav.js
@@ -1,48 +1,11 @@
-import { useState } from "react";
-
 export default function (params) {
-    
-    const [menuVisible, setMenuVisible] = useState(false);
-
-    // Function to toggle the menu visibility
-    function toggleMenu() {
-        setMenuVisible(prevState => !prevState);
-    }
-
-
     return (
         <>
-          <nav
-            id="myLinks"
-            className={`flex flex-col gap-6 fixed right-16 bottom-16`}
-          >
-            <div
-              className={`h-20 w-20 rounded-full bg-neutral-800 text-white text-lg shadow-2xl shadow-neutral-500 grid place-items-center 
-                hover:bg-gray-200 ${menuVisible ? 'block' : 'hidden'}`}
-            >
-              메뉴1
-            </div>
-            <div
-              className={`h-20 w-20 rounded-full bg-neutral-800 text-white text-lg shadow-2xl shadow-neutral-500 grid place-items-center 
-                hover:bg-gray-200 ${menuVisible ? 'block' : 'hidden'}`}
-            >
-              메뉴2
-            </div>
-            <div
-              className={`h-20 w-20 rounded-full bg-neutral-800 text-white text-lg shadow-2xl shadow-neutral-500 grid place-items-center 
-                hover:bg-gray-200  ${menuVisible ? 'block' : 'hidden'}`}
-            >
-              설정
-            </div>
-            <div
-                onClick={toggleMenu}
-              className={`h-20 w-20 rounded-full bg-neutral-800 text-white text-lg shadow-2xl shadow-neutral-500 grid place-items-center
-                `}
-            >
-              메뉴
-            </div>
 
-          </nav>
+        <nav className="fixed right-16 bottom-16">
+            <div onClick={'/'} className="h-20 w-20 rounded-full bg-neutral-800 text-white text-lg shadow-2xl shadow-neutral-500 grid place-items-center">
+                메뉴</div>
+        </nav>
         </>
-      );
-    }
+    )
+};
